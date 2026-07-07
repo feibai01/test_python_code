a676229989d3d545b13b696b95eccb40bab93f28
feibai <feibai15607935987@qq.com>
2026-07-07 19:51:31 +0800

    修改test_pillow代码

--- test_pillow.py
+++ test_pillow.py
@@ -1,4 +1,6 @@
 # 从PIL模块中导入Image类
 from PIL import Image
 # 使用Image类的open()方法打开图片，赋值给变量img
-img = Image.open("/Users/yequ/team.png")
+picture=r"C:\Users\feiba\Desktop\抠图\IMG_053_01.jpg"
+img = Image.open(picture)
+img.show()
