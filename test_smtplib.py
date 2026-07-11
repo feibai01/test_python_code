23dddb2efe2347afcf23b71a2afdf16ded267f5a
feibai <feibai15607935987@qq.com>
2026-07-11 23:44:24 +0800

    导入MIMEImage类

--- test_smtplib.py
+++ test_smtplib.py
@@ -6,6 +6,8 @@ from email.mime.multipart import MIMEMultipart
 from email.header import Header
 # 从email.mime.text中导入MIMEText类
 from email.mime.text import MIMEText
+# 从email.mime.image中导入MIMEImage类
+from email.mime.image import MIMEImage
 
 # 连接邮箱服务器：使用smtplib模块的类SMTP_SSL，创建一个实例对象qqMail
 qqMail = smtplib.SMTP_SSL("smtp.qq.com", 465)
@@ -32,3 +34,14 @@ message["To"] = Header(f"Π<{receiver}>")
 textContent = "feibai，这是插画同学绘制的团队合照，望查收~"
 # 编辑邮件正文：使用类MIMEText，创建一个实例对象mailContent
 mailContent = MIMEText(textContent, "plain", "utf-8")
+
+# 将文件路径，赋值给filePath
+filePath = r"C:\Users\feiba\Desktop\抠图\IMG_053_01.jpg"
+# 使用with open() as语句以rb的方式，打开路径为filePath的图片，并赋值给imageFile
+with open(filePath, "rb") as imageFile:
+    # 使用read()函数读取文件内容，赋值给fileContent
+    fileContent = imageFile.read()
+# 设置邮件附件：使用类MIMEImage，创建一个实例对象attachment
+attachment = MIMEImage(fileContent)
+# 调用add_header()方法，设置附件标题
+attachment.add_header("Content-Disposition", "attachment", filename="团队合照.png")
