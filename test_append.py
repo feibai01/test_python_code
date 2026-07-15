2e0a42717434e7511b108d82d09926f9c6dc43af
feibai <feibai15607935987@qq.com>
2026-07-15 16:45:47 +0800

    添加append方法使用

---
+++ test_append.py
@@ -0,0 +1,13 @@
+#定义一个空列表allInfo
+allInfo=[]
+Info1=("Max","21","female")
+Info2=("Tom","23","male")
+Info3=("Jerry","22","male")
+
+#使用append追加
+allInfo.append(Info1)
+allInfo.append(Info2)
+allInfo.append(Info3)
+
+#输出allInfo
+print(allInfo)
