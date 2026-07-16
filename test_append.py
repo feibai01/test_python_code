3c2bb0516ee3670d13000f2519d92bac657e05da
feibai <feibai15607935987@qq.com>
2026-07-16 15:30:24 +0800

    增添append方法使用方式

--- test_append.py
+++ test_append.py
@@ -1,13 +1,28 @@
 #定义一个空列表allInfo
-allInfo=[]
-Info1=("Max","21","female")
-Info2=("Tom","23","male")
-Info3=("Jerry","22","male")
+#allInfo=[]
+#Info1=("Max","21","female")
+#Info2=("Tom","23","male")
+#Info3=("Jerry","22","male")
 
 #使用append追加
-allInfo.append(Info1)
-allInfo.append(Info2)
-allInfo.append(Info3)
+#allInfo.append(Info1)
+#allInfo.append(Info2)
+#allInfo.append(Info3)
 
 #输出allInfo
-print(allInfo)
+#print(allInfo)
+
+# 设置列表nameList
+nameList = ["xiaofang", "xiaofei", "xiaobai"]
+# 空列表newList
+newList = []
+
+# TODO for循环遍历列表nameList，将元素赋值给name
+for name in nameList:
+    # 字符串拼接"Hi~"+name,赋值给word
+    word="Hi~"+name
+    # 将word追加到列表newList
+    newList.append(word)
+
+# print()输出newList
+print(newList)
