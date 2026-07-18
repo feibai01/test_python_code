359279ba9f2b846724904ee53df01b3cda057ba9
feibai <feibai15607935987@qq.com>
2026-07-18 16:32:14 +0800

    增加鱼与驴计算问题

--- code_homework_check.py
+++ code_homework_check.py
@@ -1,3 +1,4 @@
+#检查合格
 #定义字典classStu
 #classStu={"LiQiang":81,"Tony":97,"Tim":76,"Kim":97,"WangCai":44,"ShuFen":90,"Max":62,"DaHuang":55,"Kevin":85,"Gary":74}
 #for循环遍历字典
@@ -7,19 +8,39 @@
         #用格式化输出  XX同学加油！
         #print(f"{name}同学加油！")
 
+#罚抄单词,陈陈和婷婷被罚写单词，他们两个想要比较写的内容是否完全一样。
 # 婷婷的单词表
-letters1 = ["a", "bc", "def", "g", "hinj"]
+#letters1 = ["a", "bc", "def", "g", "hinj"]
 # 陈陈的单词表
-letters2 = ["abc", "defg", "h", "injs"]
-a=""
-b=""
+#letters2 = ["abc", "defg", "h", "injs"]
+#a=""
+#b=""
 #用for循环遍历列表，拼接字符串
-for n in letters1:
-    a=a+n
-for m in letters2:
-    b=b+m
+#for n in letters1:
+    #a=a+n
+#for m in letters2:
+    #b=b+m
 #使用if判断
-if a==b:
-    print("赶快写作业")
-else:
-    print("去找老师评评理")
+#if a==b:
+    #print("赶快写作业")
+#else:
+    #print("去找老师评评理")
+
+#“红鲤鱼与绿鲤鱼与驴”这句话中，到底几条鱼几头驴呢？[1.0]
+# 定义红鲤鱼与绿鲤鱼与驴列表
+#word = ["红","鲤","鱼","与","绿","鲤","鱼","与","驴"]
+#kindDict={}
+#num=0
+#num2=0
+#for a,b in zip(word,word):
+    #if a=="鱼":
+        #num=num+1
+    #if b=="驴":
+        #num2=num2+1
+#kindDict["鱼"]=num
+#kindDict["驴"]=num2
+#print(kindDict)
+
+
+    
+    
