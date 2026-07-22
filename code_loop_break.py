176184645dd41c6b14ceea35f9e2372a09377b87
feibai <feibai15607935987@qq.com>
2026-07-22 20:50:09 +0800

    增加打卡日历代码

--- code_loop_break.py
+++ code_loop_break.py
@@ -13,17 +13,26 @@
 # 王老师的要求有两个：1. 不要重量小于4kg的西瓜；2. 按列表顺序挑选够3个西瓜就停止挑选。
 # 请运用break和continue写一段代码为王老师挑选西瓜，并以列表的形式输出这3个西瓜的重量。
 # 定义melonlist列表用于保存超市的西瓜重量
-melonList = [4, 3.5, 2, 5, 6, 3, 4.7, 5.3]
+#melonList = [4, 3.5, 2, 5, 6, 3, 4.7, 5.3]
 #定义一个用于计数的变量
-a=0
+#a=0
 #定义一个用于输出三个西瓜重量的列表
-buylist=[]
-for i in melonList:
-    if i < 4:           #if语句判断西瓜重量是否小于4
-        continue        #满足条件即进入下一个循环
-    else:
-        buylist.append(i)     #若不满足条件则将西瓜重量用append函数追加至列表中
-        a=a+1           #列表追加一个，计数器加一
-        if a==3:        #if语句判断西瓜数量n是否等于3
-            break       #满足条件则跳出for循环
-print(buylist)
+#buylist=[]
+#for i in melonList:
+    #if i < 4:           #if语句判断西瓜重量是否小于4
+        #continue        #满足条件即进入下一个循环
+    #else:
+        #buylist.append(i)     #若不满足条件则将西瓜重量用append函数追加至列表中
+        #a=a+1           #列表追加一个，计数器加一
+        #if a==3:        #if语句判断西瓜数量n是否等于3
+            #break       #满足条件则跳出for循环
+#print(buylist)
+
+#记录为0则当天未背单词，记录大于0则当天已背单词。
+record=[60,0,73,139,64,48,73,63,0,59,100,121,44,30,0,0,43,67,64,51,106,0,80,0,119,59,0,58,100,90]
+count=0
+for i in record:
+    if i!=0:
+        continue
+    count+=1
+print(f"这个月有{count}天没有背单词")
