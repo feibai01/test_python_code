f60c71faf484c5833d39df39611d22eba35ae5c7
feibai <feibai15607935987@qq.com>
2026-07-26 17:49:25 +0800

    更新code_string_operations.py代码

--- code_string_operations.py
+++ code_string_operations.py
@@ -2,4 +2,14 @@
 def poetry():
     print("床前明月光，疑是地上霜。\n举头望明月，低头思故乡。")
 #调用potery函数
-poetry()
+#poetry()
+
+#定义一个函数myLen，其功能是计算并输出该字符串的长度。
+# 注意，函数内需要通过for循环遍历该字符串来统计长度。
+string="https"
+#定义函数mylen，用于输出指定字符串长度
+def myLen():
+    a=len(string)
+    print(a)
+#调用mylen函数
+myLen()
