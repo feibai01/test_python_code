1a9e102b7d7cf2395deae478049bacd073a14152
feibai <feibai15607935987@qq.com>
2026-08-08 18:11:15 +0800

    增加movies.csv代码

--- code_movie_recommendation.py
+++ code_movie_recommendation.py
@@ -2,15 +2,24 @@
 import pandas as pd  
 
 # TODO 使用read_csv()函数，sep参数，header参数，names参数和usecols参数
-# 读取路径为r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\ratings.csv"的文件
+# 读取路径为"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\ratings.csv"的文件
 # 设置参数分隔符号sep="::"
 # 传入参数header = None和names = ["用户id","电影id","评分","评分时间"]，设置columns
 # 传入参数usecols = ["用户id","电影id","评分"]，读取指定列
 # 将结果赋值给变量ratings
 ratings=pd.read_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\ratings.csv",sep="::",header=None,names=["用户id","电影id","评分","评分时间"],usecols=["用户id","电影id","评分"])
 
-# TODO 使用to_csv()函数，将处理好的ratings保存至r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\ratings.csv"，传入参数index = False不将行索引信息写入第一列
+# TODO 使用to_csv()函数，将处理好的ratings保存至"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\ratings.csv"，传入参数index = False不将行索引信息写入第一列
 ratings.to_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\ratings.csv",index=False)
 
 # 输出查看ratings
-print(ratings)
+#print(ratings)
+
+# 读取路径为"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie/movies.csv"的文件
+movies = pd.read_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie/movies.csv",usecols = ["电影id","电影名"])
+
+# 将处理好的movies保存至"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie/movies.csv"
+movies.to_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie/movies.csv",index = False)
+
+# 输出查看movies
+print(movies)
