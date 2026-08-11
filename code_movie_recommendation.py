8484f54c6e5aab3227fc8a26aba6d36c0646c431
feibai <feibai15607935987@qq.com>
2026-08-11 22:40:33 +0800

    修改造成数据透视表函数pivot_table()代码运行失败的注释错误

--- code_movie_recommendation.py
+++ code_movie_recommendation.py
@@ -2,32 +2,46 @@
 import pandas as pd  
 
 # TODO 使用read_csv()函数，sep参数，header参数，names参数和usecols参数
-# 读取路径为"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\ratings.csv"的文件
+# 读取路径为"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movie\ratings.csv"的文件
 # 设置参数分隔符号sep="::"
 # 重点增加 engine="python" 支持 sep="::"
 
 # 传入参数header = None和names = ["用户id","电影id","评分","评分时间"]，设置columns
-# 传入参数usecols = ["用户id","电影id","评分"]，读取指定列
+# 传入参数usecols = ["用户id","电影id","评分"]，读取指定列,若不传入则以names为准
 # 将结果赋值给变量ratings
-ratings=pd.read_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\ratings.csv",sep="::",header=None,names=["用户id","电影id","评分","评分时间"],usecols=["用户id","电影id","评分"],engine="python")
+#ratings=pd.read_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movie\ratings.csv",
+                    #sep="::",
+                    #header=None,
+                    #names=["用户id","电影id","评分","评分时间"],
+                    #usecols=["用户id","电影id","评分"],
+                    #engine="python"
+                    #)
 
-# TODO 使用to_csv()函数，将处理好的ratings保存至"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\ratings01.csv"，传入参数index = False不将行索引信息写入第一列
-ratings.to_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\ratings01.csv",index=False)
+# TODO 使用to_csv()函数，将处理好的ratings保存至"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movie\ratings01.csv"，传入参数index = False不将行索引信息写入第一列
+#ratings.to_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movie\ratings01.csv",index=False)
 
 # 输出查看ratings
 #print(ratings)
 
-# 读取路径为"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movies.csv"的文件
-movies = pd.read_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movies.csv",usecols = ["电影id","电影名"])
+# 读取路径为"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movie\movies.csv"的文件
+#movies = pd.read_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movie\movies.csv",
+                     #usecols = ["电影id","电影名"]
+                     #)
 
-# 将处理好的movies保存至"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movies01.csv"
-movies.to_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movies01.csv",index = False)
+# 将处理好的movies保存至"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movie\movies01.csv"
+#movies.to_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movie\movies01.csv",index = False)
 
 # 输出查看movies
 #print(movies)
 
+#####################################################################
+
+# 读取并处理路径为r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movie\ratings01.csv"的数据集，将结果赋值给变量ratings
+ratings01 = pd.read_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movie\ratings01.csv")
+# 读取路径为r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movie\movies01.csv"的数据集，将结果赋值给变量movies
+movies01 = pd.read_csv(r"D:\tools\编程\VSCODE\python_vscode\练习所需文件\movie\movie\movies01.csv")
 # TODO 使用merge()函数将ratings和movies按照电影id这一列连接起来
-movieRatings=pd.merge(ratings,movies)
+movieRatings=pd.merge(ratings01,movies01)
 
 # 输出查看movieRatings
 #print(movieRatings)
